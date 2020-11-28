8e08992eab2a5755d41f9f1fe8f5623c4fe2ac20
Seindi Rahmat Barus <seindi@outlook.com>
2020-11-28 14:45:26 +0700

    .

--- bin/package/postgre-sql.sh
+++ bin/package/postgre-sql.sh
@@ -5,4 +5,9 @@ if [ "$1" == "configure" ]
 		sudo -u postgres psql -c "alter user postgres with password 'postgres';"
 		sudo -u postgres psql -c "create database master;"
 		sudo -u postgres psql -c "create database client;"
+elif [ "$1" == "start" ]
+	then
+		systemctl start postgresql
+else
+	echo ""
 	fi
