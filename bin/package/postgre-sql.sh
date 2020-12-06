6492e611c06efe9fd681c44d48fe69a984ff5422
Seindi Rahmat Barus <seindi@outlook.com>
2020-12-07 05:59:47 +0700

    .

--- bin/package/postgre-sql.sh
+++ bin/package/postgre-sql.sh
@@ -5,9 +5,18 @@ if [ "$1" == "configure" ]
 		sudo -u postgres psql -c "alter user postgres with password 'postgres';"
 		sudo -u postgres psql -c "create database master;"
 		sudo -u postgres psql -c "create database client;"
+elif [ "$1" == "enable" ]
+	then
+		systemctl enable postgresql
+elif [ "$1" == "disable" ]
+	then
+		systemctl disable postgresql
 elif [ "$1" == "start" ]
 	then
 		systemctl start postgresql
+elif [ "$1" == "stop" ]
+	then
+		systemctl stop postgresql
 else
 	echo ""
 	fi
